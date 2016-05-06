70f24d31dd77da9632aa5df6c5dd4c3d950c4749
ilnaw <wanlilikekobe@outlook.com>
2016-05-06 14:23:46 +0800

    Initial Commit

---
+++ WLAreaPickerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WLAreaPickerView
+//
+//  Created by 万里 on 16/5/6.
+//  Copyright © 2016年 wanli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
