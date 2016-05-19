1dd5929cdbc46ec961b9586b2befbc4b3f149377
ilnaw <wanlilikekobe@outlook.com>
2016-05-19 16:57:36 +0800

    g

--- WLAreaPickerView/AppDelegate.swift
+++ WLAreaPickerView/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         print("1")
         print("YES")
+        print("not")
         return true
     }
 
